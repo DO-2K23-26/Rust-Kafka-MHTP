0993a0e9c224072f6d32b70cfad43b0aa516ebab
Théo Tchilinguirian <theo.tchlx@gmail.com>
2025-01-13 18:31:30 +0100

    feat(merger): working consumer

--- src/merger/main.rs
+++ src/merger/main.rs
@@ -1,13 +1,11 @@
 use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 
-fn main() {
-  println!("Hi. I will be your merger in this project.");
-
+fn create_consumer() {
   let mut consumer =
      Consumer::from_hosts(vec!("localhost:9092".to_owned()))
-        .with_topic_partitions("my-topic".to_owned(), &[0, 1])
+        .with_topic_partitions("orders".to_owned(), &[0, 1])
         .with_fallback_offset(FetchOffset::Earliest)
-        .with_group("my-group".to_owned())
+        .with_group("merger".to_owned())
         .with_offset_storage(Some(GroupOffsetStorage::Kafka))
         .create()
         .unwrap();
@@ -21,4 +19,36 @@ fn main() {
     consumer.commit_consumed().unwrap();
   }
 }
+/*
+use std::fmt::Write;
+use std::time::Duration;
+use kafka::producer::{Producer, Record, RequiredAcks};
 
+fn create_producer() {
+  let mut producer = Producer::from_hosts(vec!("localhost:9092".to_owned()))
+    .with_ack_timeout(Duration::from_secs(1))
+    .with_required_acks(RequiredAcks::One)
+    .create()
+    .unwrap();
+
+  let mut buf = Vec::new();
+  write!(&mut buf, "message").unwrap();
+  producer.send(&Record {
+    topic: "my-topic",
+    partition: 0,
+    key: "key",
+    value: &buf,
+  }).unwrap();
+
+  let mut buf = String::with_capacity(2);
+  for i in 0..10 {
+    let _ = write!(&mut buf, "{}", i); // some computation of the message data to be sent
+    producer.send(&Record::from_value("my-topic", buf.as_bytes())).unwrap();
+    buf.clear();
+  }
+}*/
+
+fn main() {
+  create_consumer();
+  //create_producer();
+}
