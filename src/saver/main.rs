3d811eb299cfdf0e43e4136036d57bd215b74201
pauline <pauline.contat01@etu.umontpellier.fr>
2025-01-13 18:04:08 +0100

    Add Kafka and DataFusion dependencies, and update main.rs in saver module

--- src/saver/main.rs
+++ src/saver/main.rs
@@ -1,3 +1,16 @@
-pub fn main(){
-    println!("Hello, world!");
+use Rust_Kafka_MHT::common::consumer;
+use datafusion::prelude::*;
+
+fn main()-> Result<(), Box<dyn std::error::Error>> {
+    let mut consumer = consumer::get_consumer("first-course")?;
+    loop {
+    for ms in consumer.poll().unwrap().iter() {
+      for m in ms.messages() {
+        let str = String::from_utf8_lossy(m.value);
+        println!("{:?}",str); // put message in datafusion table
+      }
+      let _ = consumer.consume_messageset(ms);
+    }
+    consumer.commit_consumed().unwrap();
+  }
 }
