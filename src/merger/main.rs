cd7682b2491a7f34cf6a1e900925f4b16640e126
Théo Tchilinguirian <theo.tchlx@gmail.com>
2025-01-14 11:22:23 +0100

    feat(merger): init producer
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- src/merger/main.rs
+++ src/merger/main.rs
@@ -1,6 +1,7 @@
 use Rust_Kafka_MHT::merger::consumer::create_consumer;
+use Rust_Kafka_MHT::merger::producer::create_producer;
 
 fn main() {
+  create_producer();
   create_consumer();
-  //create_producer();
 }
