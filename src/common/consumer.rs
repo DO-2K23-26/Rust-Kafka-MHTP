975d3633b5beccbbd96e26ff7607d97efbdf1030
pauline <pauline.contat01@etu.umontpellier.fr>
2025-01-14 14:53:13 +0100

    Add Minio service and create data directory
    Update Kafka consumer
    
    Add datafusion code to save Kafka messages to Parquet file

--- src/common/consumer.rs
+++ src/common/consumer.rs
@@ -1,12 +1,16 @@
-use kafka::{client::GroupOffsetStorage, consumer::{Consumer, FetchOffset}};
+use kafka::{
+    client::GroupOffsetStorage,
+    consumer::{Consumer, FetchOffset},
+};
 
-pub fn get_consumer(topic: &str) -> Result<Consumer, kafka::error::Error> {
-    let consumer = Consumer::from_hosts(vec!("localhost:9092".to_owned()))
-        .with_topic_partitions(topic.to_owned(), &[0])
+pub fn get_consumer(topic: &str) -> Result<Consumer, kafka::Error> {
+    let consumer = Consumer::from_hosts(vec!["localhost:9092".to_string()])
+        // .with_topic_partitions(topic.to_owned(), &[0])
+        .with_topic(topic.to_string())
         .with_fallback_offset(FetchOffset::Earliest)
         .with_offset_storage(Some(GroupOffsetStorage::Kafka))
-        .with_group("group".to_owned())
-        .create();
+        .with_group("order-consumer-group".to_string())
+        .create()?;
     
-    consumer.map_err(|err| err.into())
+    Ok(consumer)
 }
--- src/saver/main.rs
+++ src/saver/main.rs
@@ -1,16 +1,46 @@
-use Rust_Kafka_MHT::common::consumer;
+use datafusion::arrow::array::{Array, StringArray};
+use datafusion::arrow::datatypes::{DataType, Field, Schema};
+use datafusion::arrow::record_batch::RecordBatch;
+use datafusion::physical_plan::memory::MemoryExec;
+use datafusion::prelude::SessionContext;
 use datafusion::prelude::*;
+use std::sync::Arc;
+use Rust_Kafka_MHT::common::consumer;
 
-fn main()-> Result<(), Box<dyn std::error::Error>> {
-    let mut consumer = consumer::get_consumer("first-course")?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut consumer = consumer::get_consumer("Order")?;
+    let ctx = SessionContext::new();
+    let schema = Arc::new(Schema::new(vec![Field::new(
+        "message",
+        DataType::Utf8,
+        false,
+    )]));
+
     loop {
-    for ms in consumer.poll().unwrap().iter() {
-      for m in ms.messages() {
-        let str = String::from_utf8_lossy(m.value);
-        println!("{:?}",str); // put message in datafusion table
-      }
-      let _ = consumer.consume_messageset(ms);
+        let mut messages = Vec::new();
+        for ms in consumer.poll().unwrap().iter() {
+            for m in ms.messages() {
+                let message_text = String::from_utf8_lossy(m.value).to_string();
+                println!("{:?}", message_text);
+                messages.push(message_text);
+            }
+            let _ = consumer.consume_messageset(ms);
+        }
+        consumer.commit_consumed().unwrap();
+        if !messages.is_empty() {   
+            let array = Arc::new(StringArray::from(messages)) as Arc<dyn Array>;
+            let record_batch = RecordBatch::try_new(schema.clone(), vec![array])?;
+            let execution_plan = Arc::new(MemoryExec::try_new(
+                &[vec![record_batch]],
+                schema.clone(),
+                None,
+            )?);
+
+            ctx.write_parquet(execution_plan, "kafka_output.parquet", None)
+                .await?;
+            let dataframe = ctx.read_parquet("kafka_output.parquet", ParquetReadOptions::default()).await?;
+            dataframe.show().await?;
+        }
     }
-    consumer.commit_consumed().unwrap();
-  }
 }
