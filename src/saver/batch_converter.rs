8ed5db398288b0843484c1165b342a9e7693abf9
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-01-15 11:49:38 +0100

    feat:convert sold cars into column values

--- src/saver/batch_converter.rs
+++ src/saver/batch_converter.rs
@@ -2,7 +2,7 @@ use std::{error::Error, sync::Arc};
 
 use datafusion::{
     arrow::{
-        array::{RecordBatch, StringArray},
+        array::{Float64Array, Int64Array, RecordBatch, StringArray},
         datatypes::{DataType, Field, Schema},
     },
     physical_plan::memory::MemoryExec,
@@ -17,11 +17,16 @@ pub fn convert(sold_cars: Vec<SoldCar>) -> Result<Arc<MemoryExec>, Box<dyn Error
     )]));
 
     let array_id: Vec<String> = sold_cars.iter().map(|m| m.id.to_string()).collect();
+    let array_brand: Vec<String> = sold_cars.iter().map(|m| m.brand.to_string()).collect();
+    let array_price: Vec<f64> = sold_cars.iter().map(|m| m.price).collect();
+    let array_created_at: Vec<i64> = sold_cars.iter().map(|m| m.created_at).collect();
 
     let df_array_id = Arc::new(StringArray::from(array_id));
+    let df_array_brand = Arc::new(StringArray::from(array_brand));
+    let df_array_price = Arc::new(Float64Array::from(array_price));
+    let df_array_created_at = Arc::new(Int64Array::from(array_created_at));
 
-    let record_batch = RecordBatch::try_new(schema.clone(), vec![df_array_id])?;
-
+    let record_batch = RecordBatch::try_new(schema.clone(), vec![df_array_id, df_array_brand, df_array_price, df_array_created_at])?;
     let execution_plan = Arc::new(MemoryExec::try_new(
         &[vec![record_batch]],
         schema.clone(),
