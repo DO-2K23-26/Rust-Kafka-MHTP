62dc18bc5f81719899fa34d06a3c38da99b30357
Théo Tchilinguirian <theo.tchlx@gmail.com>
2025-01-15 08:35:09 +0100

    refactor(merge): convert to standard tab size
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- src/merger/producer.rs
+++ src/merger/producer.rs
@@ -1,25 +1,27 @@
-use std::time::Duration;
-use kafka::producer::{Producer, Record, RequiredAcks};
 use bincode;
+use kafka::producer::{Producer, Record, RequiredAcks};
+use std::time::Duration;
 
-use crate::common::types::{SoldCar, Brand};
+use crate::common::types::{Brand, SoldCar};
 
 pub fn create_producer() {
-  let mut producer = Producer::from_hosts(vec!("localhost:9092".to_owned()))
-    .with_ack_timeout(Duration::from_secs(1))
-    .with_required_acks(RequiredAcks::One)
-    .create()
-    .unwrap();
+    let mut producer = Producer::from_hosts(vec!["localhost:9092".to_owned()])
+        .with_ack_timeout(Duration::from_secs(1))
+        .with_required_acks(RequiredAcks::One)
+        .create()
+        .unwrap();
 
-  for _ in 0..10 {
-    // Here call the merge logic function
-    let pwetter = SoldCar {
-      id: 1,
-      brand: Brand::FERRARI,
-      price: 100.0190382110349729845762347,
-      created_at: 1234567890,
-    };
-    let encoded: Vec<u8> = bincode::serialize(&pwetter).unwrap();
-    producer.send(&Record::from_value("orders", encoded.as_slice())).unwrap();
-  }
+    for _ in 0..10 {
+        // Here call the merge logic function
+        let pwetter = SoldCar {
+            id: 1,
+            brand: Brand::FERRARI,
+            price: 100.0190382110349729845762347,
+            created_at: 1234567890,
+        };
+        let encoded: Vec<u8> = bincode::serialize(&pwetter).unwrap();
+        producer
+            .send(&Record::from_value("orders", encoded.as_slice()))
+            .unwrap();
+    }
 }
