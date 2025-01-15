46b49f4a9b3e3d2b3367cc60e5a661bad0c503c0
Théo Tchilinguirian <theo.tchlx@gmail.com>
2025-01-15 09:02:43 +0100

    feat(logic): adds logic functions
    This commit adds the check_brand(), calculate_price() and merge()
    functions.
    
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- src/merger/logic.rs
+++ src/merger/logic.rs
@@ -1,1 +1,28 @@
 /// Handles the merge logic.
+use std::time::SystemTime;
+
+use crate::common::types::{Chassis, Brand, Order, Wheel, SoldCar};
+
+pub fn check_brand(order: Order, chassis: Chassis) -> bool {
+    order.brand == chassis.brand
+}
+
+pub fn calculate_price(order: Order, chassis: Chassis, wheels: [Wheel; 4], brand: Brand) -> f64 {
+    let mut price = order.price + chassis.price;
+    for wheel in wheels.iter() {
+        price += wheel.price;
+    }
+    if brand == Brand::FERRARI {  // The price of luxury.
+        price *= 1.2;
+    }
+    price
+}
+
+pub fn merge(order: Order, chassis: Chassis, brand: &Brand, wheels: [Wheel; 4]) -> SoldCar {
+    SoldCar {
+        id: order.id,
+        brand: brand.clone(),  // Order brand and chassis brand must be the same before merging.
+        price: calculate_price(order, chassis, wheels, brand.clone()),
+        created_at: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
+    }
+}
