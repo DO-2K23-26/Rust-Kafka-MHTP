bce0c25c8f2214eebb199c3fbedcd56efb7799ad
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-01-15 18:38:46 +0100

    feat:link to minio

--- src/saver/main.rs
+++ src/saver/main.rs
@@ -1,5 +1,8 @@
-use datafusion::parquet::file::properties::WriterProperties;
+use std::sync::Arc;
+
+use datafusion::execution::object_store::ObjectStoreUrl;
 use datafusion::prelude::SessionContext;
+use object_store::aws::AmazonS3Builder;
 // use aws_credential_types::provider::Credentials;
 use Rust_Kafka_MHT::common::consumer;
 use Rust_Kafka_MHT::common::types::SoldCar;
@@ -13,32 +16,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut consumer = consumer::get_consumer("SoldCar")?;
     let ctx = SessionContext::new();
 
-    // const MINIO_ACCESS_KEY_ID: &str = "AKIAIOSFODNN7EXAMPLE";
-    // const MINIO_SECRET_ACCESS_KEY: &str = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-    // const PROVIDER_NAME: &str = "Static";
-    // const MINIO_ENDPOINT: &str = "http://localhost:9001";
+    const MINIO_ACCESS_KEY_ID: &str = "6UKbinEQGNB7hh2ftxMo";
+    const MINIO_SECRET_ACCESS_KEY: &str = "MHASPodqbmFH9tgeGWE4hHiJYkBnWsCTQuxJvLEq";
 
-    // let s3_file_system = S3FileSystem::new(
-    //     Some(SharedCredentialsProvider::new(Credentials::new(
-    //         MINIO_ACCESS_KEY_ID,
-    //         MINIO_SECRET_ACCESS_KEY,
-    //         None,
-    //         None,
-    //         PROVIDER_NAME,
-    //     ))), // Credentials provider
-    //     None,                                                        // Region
-    //     Some(Endpoint::immutable(Uri::from_static(MINIO_ENDPOINT))), // Endpoint
-    //     None,                                                        // RetryConfig
-    //     None,                                                        // AsyncSleep
-    //     None,                                                        // TimeoutConfig
-    // )
-    // .await;
-    // let s3_url = Url::parse("s3://")?;
-    // let s3_store: Arc<dyn ObjectStore> = Arc::new(s3_file_system);
-    // ctx.register_object_store(&s3_url, s3_store);
+    let s3 = AmazonS3Builder::new()
+        .with_region("us-east-1")
+        .with_bucket_name("datafusion")
+        .with_access_key_id(MINIO_ACCESS_KEY_ID)
+        .with_secret_access_key(MINIO_SECRET_ACCESS_KEY)
+        .with_endpoint("http://localhost:9000/")
+        .with_allow_http(true)
+        .build()?;
+    let object_store_url = ObjectStoreUrl::parse("s3://datafusion/").unwrap();
+    ctx.register_object_store(&object_store_url.as_ref(), Arc::new(s3));
     let mut sold_cars: [SoldCar; MAX_BATCH_SIZE] = [SoldCar::default(); MAX_BATCH_SIZE];
     let mut cursor = 0;
-
 
     loop {
         for ms in consumer.poll().unwrap().iter() {
@@ -54,13 +46,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 cursor += 1;
                 if cursor == MAX_BATCH_SIZE {
                     let execution_plan = batch_converter::convert(sold_cars.to_vec())?;
-                    ctx.write_parquet(execution_plan, "output", None).await?;
+                    ctx.write_parquet(execution_plan, "s3://datafusion/output/", None).await?;
+                    println!("wrote parquet");
                     cursor = 0;
                 };
             }
             let _ = consumer.consume_messageset(ms);
             consumer.commit_consumed().unwrap();
         }
-        println!("{}",cursor)
     }
 }
