daa8876382f0974684a384372bf01bc32233063e
duratm <duratmathias@gmail.com>
2025-01-16 14:32:42 +0100

    feat(partition): Handle multiple partitions for storage and store data in the minio with the right filenames

--- src/common/consumer.rs
+++ src/common/consumer.rs
@@ -3,9 +3,9 @@ use kafka::{
     consumer::{Consumer, FetchOffset},
 };
 
-pub fn get_consumer(topic: &str) -> Result<Consumer, kafka::Error> {
+pub fn get_consumer(topic: &str, partition_id: i32) -> Result<Consumer, kafka::Error> {
     let consumer = Consumer::from_hosts(vec!["localhost:19092".to_string(),"localhost:29092".to_string()])
-        .with_topic_partitions(topic.to_owned(), &[0])
+        .with_topic_partitions(topic.to_owned(), &[partition_id])
         .with_fallback_offset(FetchOffset::Earliest)
         .with_offset_storage(Some(GroupOffsetStorage::Kafka))
         .with_group("order-consumer-group".to_string())
