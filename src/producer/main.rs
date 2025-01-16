8b0d21c646ec487e6b043b9adddfc760918980ec
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-01-16 15:23:19 +0100

    fix:correct schema value

--- src/producer/main.rs
+++ src/producer/main.rs
@@ -1,5 +1,3 @@
-
-
 use std::{
     sync::{Arc, RwLock},
     time::Duration,
@@ -9,25 +7,30 @@ use kafka::{
     client::RequiredAcks,
     producer::{Producer, Record},
 };
-    use schema_register::register;
-    use schema_registry_converter::{async_impl::schema_registry::SrSettings, schema_registry_common::SubjectNameStrategy};
-    use schema_registry_converter::async_impl::avro::AvroEncoder;
+use schema_register::register;
+use schema_registry_converter::async_impl::avro::AvroEncoder;
+use schema_registry_converter::{
+    async_impl::schema_registry::SrSettings, schema_registry_common::SubjectNameStrategy,
+};
 
-    use serde_avro_derive::BuildSchema;
-    use tokio::task::JoinSet;
+use serde_avro_derive::BuildSchema;
+use tokio::task::JoinSet;
 use Rust_Kafka_MHT::common::types::{Chassis, Emittable, Order, SoldCar, Wheel};
 
 mod schema_register;
 
 pub fn launch_producer<T>(producer_set: Arc<RwLock<JoinSet<()>>>)
 where
-    T: Emittable + Send + Sync + BuildSchema +'static,
+    T: Emittable + Send + Sync + BuildSchema + 'static,
 {
-    let mut producer = Producer::from_hosts(vec!["localhost:19092".to_owned(),"localhost:29092".to_owned()])
-        .with_ack_timeout(Duration::from_secs(1))
-        .with_required_acks(RequiredAcks::One)
-        .create()
-        .unwrap();
+    let mut producer = Producer::from_hosts(vec![
+        "localhost:19092".to_owned(),
+        "localhost:29092".to_owned(),
+    ])
+    .with_ack_timeout(Duration::from_secs(1))
+    .with_required_acks(RequiredAcks::One)
+    .create()
+    .unwrap();
     let mut producer_lock = producer_set.write().unwrap();
     producer_lock.spawn(async move {
         let sr_settings = SrSettings::new(String::from("http://localhost:8085"));
@@ -35,17 +38,14 @@ where
         loop {
             let topic_name = T::get_topic_name();
             let client_payload = encoder
-            .encode_struct(
-                &T::generate(),
-                &SubjectNameStrategy::TopicNameStrategy("Client".to_string(), false),
-            )
-            .await
-            .expect("Failed to encode client object");
-            
-            let _ = producer.send(&Record::from_value(
-                &topic_name,
-                client_payload
-            ));
+                .encode_struct(
+                    &T::generate(),
+                    &SubjectNameStrategy::TopicNameStrategy(T::get_topic_name(), false),
+                )
+                .await
+                .expect("Failed to encode client object");
+
+            let _ = producer.send(&Record::from_value(&topic_name, client_payload));
             std::thread::sleep(Duration::from_millis(T::get_frequency()));
         }
     });
@@ -60,7 +60,7 @@ async fn main() {
 
     let producer_set = JoinSet::new();
     let producer_arc = Arc::new(RwLock::new(producer_set));
-    
+
     launch_producer::<Order>(producer_arc.clone());
     launch_producer::<Wheel>(producer_arc.clone());
     launch_producer::<Chassis>(producer_arc.clone());
